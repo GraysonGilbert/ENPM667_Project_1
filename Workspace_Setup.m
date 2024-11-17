0d02943926c913b89ed3e2a8320ac727f41e279b
Tim Sweeney <timsweeney97@gmail.com>
2024-11-17 17:22:28 -0500

    Got kalman filter setup configured.  Not producing accurate estimates yet

--- Workspace_Setup.m
+++ Workspace_Setup.m
@@ -19,7 +19,22 @@ P_0 = [100, 0, 0;
        0, 100, 0;
        0, 0, 100;];
 
-xhat_0 = [5;2;3];
+xhat_0_1 = 5;
+xhat_0_2 = 2;
+xhat_0_3 = 3;
+xhat_0 = [xhat_0_1;
+          xhat_0_2;
+          xhat_0_3];
 
-% Calculate K_kal_0
-C_0 = []
+% Calculate C_0 matrix
+C11 = cosd(xhat_0_2);
+C12 = -xhat_0_1 * sind(xhat_0_2);
+C13 = 0;
+C21 = cosd(xhat_0_2 - xhat_0_3 + 0);  % 0 represents uk-1
+C22 = -xhat_0_1 * sind(xhat_0_2 - xhat_0_3 + 0);
+C23 = xhat_0_1 * sind(xhat_0_2 - xhat_0_3 + 0);
+C_0 = [C11, C12, C13;
+       C21, C22, C23;]
+
+% Calculate K_kal_0 matrix
+K_kal_0 = P_0 * C_0' * inv(C_0 * P_0 * C_0' + R_EKF) 
