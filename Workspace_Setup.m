d14ad0ff3e20b38833a7296ea685353e07f9a9e3
Tim Sweeney <timsweeney97@gmail.com>
2024-11-22 23:25:26 -0500

    Saving progress

--- Workspace_Setup.m
+++ Workspace_Setup.m
@@ -9,9 +9,9 @@ B = [0;
 
 % Given Parameters
 
-Q_EKF = [0.0025, 0, 0; 
-         0, 0.0025, 0; 
-         0, 0, 0.0025];
+Q_EKF = [0.025, 0, 0; 
+         0, 0.025, 0; 
+         0, 0, 0.025];
 
 R_EKF = [1, 0;
          0, 1;];
@@ -20,8 +20,8 @@ P_0 = [100, 0, 0;
        0, 100, 0;
        0, 0, 100;];
 
-xhat_0_1 = 5;
-xhat_0_2 = 7;
+xhat_0_1 = 8;
+xhat_0_2 = 5;
 xhat_0_3 = 0;
 xhat_0 = [xhat_0_1;
           xhat_0_2;
@@ -35,7 +35,10 @@ C21 = cosd(xhat_0_2 - xhat_0_3 + 0);  % 0 represents uk-1
 C22 = -xhat_0_1 * sind(xhat_0_2 - xhat_0_3 + 0);
 C23 = xhat_0_1 * sind(xhat_0_2 - xhat_0_3 + 0);
 C_0 = [C11, C12, C13;
-       C21, C22, C23;];
+       C21, C22, C23;]
 
 % Calculate K_kal_0 matrix
 K_kal_0 = P_0 * C_0' * inv(C_0 * P_0 * C_0' + R_EKF) 
+
+
+i=0;
