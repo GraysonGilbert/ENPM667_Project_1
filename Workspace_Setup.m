cc3d0027533fb1d51e62b45a1c8f2ba44879a669
GraysonGilbert <91498658+GraysonGilbert@users.noreply.github.com>
2024-11-24 18:48:55 -0500

    Add files via upload
    working model

--- Workspace_Setup.m
+++ Workspace_Setup.m
@@ -1,45 +1,71 @@
 % Script to set up workspace for Simulink Model
+load('x1_timeseries.mat', 'x1_ts');
+load('x2_timeseries.mat', 'x2_ts');
+load('x3_timeseries.mat', 'x3_ts');
+load('x1_stable_timeseries.mat', 'x1_stable_ts');
+%load('psi_timeseries.mat', 'psi_ts');
+
+psi_data = readtable('X1_X2_X3_data.xlsx', 'Sheet','psi_k');
+% Extract Columns
+psi_time = psi_data{:,1};
+psi_vals = psi_data{:,2};
 
 
-%x = [(C_p * I_theta * exp(-c * d)) / d^2; phi; phi_dot];    % State Vector
+% Initial Conditions
+
+x_k = [4.9986;2.5123;2.9881];
+x1_0 = x1_ts.getsampleusingtime(0).Data;
 
 A = [ 1, 0, 0; 0, 1, 1; 0, 0, 1];
 B = [0; 0; 1];
 
 % Given Parameters
 
+% System Noise Parameters
+
 Q_sys = [0.0025, 0, 0; 0, 0.0025, 0; 0, 0, 0.0025];
 
 R_sys = 0.001;
 
-P_0 = [100, 0, 0; 0, 100, 0; 0, 0, 100];
+w = [0; 0; 0];
+
+v = 0;
+
+
+% Extended Kalman Filter Parameters
+
+prev_P_k = [100, 0, 0; 0, 100, 0; 0, 0, 100];
 
 Q_ekf = [0.0025, 0, 0; 0, 0.0025, 0; 0, 0, 0.0025];
 
 R_ekf = [1, 0; 0, 1];
 
+prev_x_hat = [5; 2; 3];
+%prev_x_hat = [2; 10; 5];
+
+G = [0.15, 0.4];     % Proportional Gain
+
+
+prev_u_k_ekf = (-G(1)*prev_x_hat(2) - (G(2)*prev_x_hat(3)));
+
+
+% LQR Parameters
+
 Q_lqr = [30, 0; 0, 1];
 
 R_lqr = 1000;
+%R_lqr = 1000;
 
-x_hat_0 = [5; 2; 3];
+K_lqr = [0.01287, 0.5764];
 
-G = [0.1, 0.4];     % Proportional Gain
+u_k_lqr = -K_lqr * [prev_x_hat(2);prev_x_hat(3)];
 
-K_lqr = [0.1287, 0.5764];
+A_lqr = [1, 1; 0, 1];
+B_lqr = [0; 1];
 
 
 
-%x_1k = ;
+%K_lqr = lqr(A_lqr, B_lqr, Q_lqr, R_lqr);
 
-%x_k = [x_1k; x_2k; x_3k];
 
-function x = myStateTransitionFcn(x, u)
 
-% dt = 0.01 %Sample Time
-
-x = 
-
-% Make function inside here
-
-end
